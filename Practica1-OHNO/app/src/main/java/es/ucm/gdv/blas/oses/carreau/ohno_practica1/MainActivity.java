097d4a8cda3ee6d4a5299228a13992dfee3af80f
raulblas7 <48948743+raulblas7@users.noreply.github.com>
2021-09-30 19:03:23 +0200

    Ahora si¿?

---
+++ Practica1-OHNO/app/src/main/java/es/ucm/gdv/blas/oses/carreau/ohno_practica1/MainActivity.java
@@ -0,0 +1,14 @@
+package es.ucm.gdv.blas.oses.carreau.ohno_practica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
