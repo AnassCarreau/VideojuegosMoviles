c77735a722a5370534df775d6cbc2f4165d2f91c
Anass <anasca01@ucm.es>
2021-11-19 18:40:24 +0100

    TOMA RULO TE TOCA

--- Practica2-FLOWFREE/Assets/Scripts/LineController.cs
+++ Practica2-FLOWFREE/Assets/Scripts/LineController.cs
@@ -9,19 +9,14 @@ public class LineController : MonoBehaviour
     Vector2 start;
     public Transform inicio;
     bool pintar = false;
+
+    Collider2D inicioCircle;
     // Start is called before the first frame update
     void Awake()
     {
-        lr = GetComponent<LineRenderer>();
+        lr =GetComponent<LineRenderer>();
         points = new List<Vector2>();
-        lr.startColor = Color.red;
-        lr.endColor = Color.red;
-    }
-
-   public void SetUpLine(Transform[] points) 
-    {
-      //  lr.positionCount = points.Length;
-       // this.points = points;
+       
     }
 
     private void Update()
@@ -32,37 +27,42 @@ public class LineController : MonoBehaviour
 
 
         // Debug.Log(inicio.position + " " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        if (ra.collider != null && ra.collider.CompareTag("Inicio"))
+        if (ra.collider != null && ra.collider.CompareTag("Inicio") && Input.GetMouseButtonDown(0))
         {
-
-            if (Input.GetMouseButtonDown(0))
-            {
                 pintar = true;
-            }
+            inicioCircle = ra.collider;
+            start = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            points.Clear();
+            points.Add(start);
+            points.Add(start);
+            lr.startColor = ra.collider.GetComponent<SpriteRenderer>().color;
+            lr.endColor = ra.collider.GetComponent<SpriteRenderer>().color;
+            lr.positionCount = 2;
         }
 
         if (pintar)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                start = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                points.Clear();
-                points.Add(start);
-                points.Add(start);
-                lr.positionCount = 2;
-
-            }
+           
             if (Input.GetMouseButton(0))
             {
+
+                //Cerrar
+
+                 ra = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+
+                // Debug.Log(inicio.position + " " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                if (ra.collider != null && ra.collider!= inicioCircle && ra.collider.CompareTag("Inicio"))
+                {
+                    pintar = false;
+                }
+
+                //
                 Vector2 actual = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //  Debug.Log(Vector2.Dot(actual, points[points.Count - 1]));
-                int ang = (int)Vector2.Angle(points[points.Count - 2], actual);
+                Vector2 vec = Vector2.MoveTowards(points[points.Count - 2], actual,10000).normalized;
+                Debug.Log(vec.x + " " + vec.y);
                 float dis = Vector2.Distance(points[points.Count - 2], actual);
-                // Debug.Log(dis);
-                // Debug.DrawLine(points[points.Count - 1], Vector2.up*100);
-                //  Debug.DrawLine(points[points.Count - 1], Vector2.left*100);
-                //  Debug.Log("Dis : " + dis +  "  Ang : " + ang);
-                if (dis > 1 /*&& (Mathf.Approximately(90.0f,ang) || Mathf.Approximately(0.0f,ang)*/ )
+              
+                if (dis > 10 /*&& (Mathf.Approximately(90.0f,ang) || Mathf.Approximately(0.0f,ang)*/ )
                 {
                     Debug.Log("hola");
                     points.Add(actual);
